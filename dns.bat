b11d14403e5a55b97f54c010a3a0ae7a47f7d344
giggityfiles <giggityfiles@gmail.com>
2025-10-14 10:38:51 -0400

    nenenenen

--- dns.bat
+++ dns.bat
@@ -13,11 +13,20 @@ set "primaryDNS=45.90.28.180"
 set "secondaryDNS=45.90.30.180"
 set "found=0"
 
+REM Function to trim leading and trailing spaces
+:trim
+set "var=%~1"
+REM Trim leading spaces
+for /f "tokens=* delims= " %%A in ("!var!") do set "var=%%A"
+REM Trim trailing spaces
+:trimloop
+if not "!var!"=="" if "!var:~-1!"==" " set "var=!var:~0,-1!" & goto trimloop
+set "%~2=!var!"
+goto :eof
+
 REM Loop through all connected network adapters
 for /f "skip=1 tokens=*" %%A in ('wmic nic where "NetEnabled=true" get NetConnectionID') do (
-    set "adapter=%%A"
-    REM Trim spaces
-    for /f "tokens=* delims= " %%X in ("!adapter!") do set "adapter=%%X"
+    call :trim "%%A" adapter
     if not "!adapter!"=="" (
         echo Updating DNS for "!adapter!"...
         netsh interface ip set dns name="!adapter!" static %primaryDNS%
@@ -33,8 +42,7 @@ if !found! equ 0 (
     echo.
     echo Verifying DNS settings...
     for /f "skip=1 tokens=*" %%A in ('wmic nic where "NetEnabled=true" get NetConnectionID') do (
-        set "adapter=%%A"
-        for /f "tokens=* delims= " %%X in ("!adapter!") do set "adapter=%%X"
+        call :trim "%%A" adapter
         if not "!adapter!"=="" (
             echo !adapter!:
             netsh interface ip show dns name="!adapter!"
