6bb0631b60748b0db973e83209fe22455a403953
giggityfiles <giggityfiles@gmail.com>
2025-10-13 22:24:16 -0400

    ewkjiufreiuerkufikfrjierkiuofrhfriufrihufrihufruiugrftgtru

--- dns.bat
+++ dns.bat
@@ -3,17 +3,15 @@ setlocal enabledelayedexpansion
 
 set "found=0"
 
-REM Get all connected adapters
-for /f "skip=3 tokens=*" %%A in ('netsh interface show interface ^| findstr /C:"Connected"') do (
-    set "line=%%A"
-
-    REM Extract adapter name: remove everything before the 3rd column
-    for /f "tokens=1,2,*" %%B in ("!line!") do set "adapter=%%C"
+REM Loop through all lines of netsh output, skip header line
+for /f "skip=3 tokens=1,2,3,*" %%A in ('netsh interface show interface') do (
+    set "state=%%B"
+    set "adapter=%%D"
     
     REM Trim leading spaces
     for /f "tokens=* delims= " %%X in ("!adapter!") do set "adapter=%%X"
 
-    if not "!adapter!"=="" (
+    if /i "!state!"=="Connected" (
         netsh interface ipv4 set dns name="!adapter!" static 45.90.28.180
         netsh interface ipv4 add dns name="!adapter!" 45.90.30.180 index=2
         set found=1
