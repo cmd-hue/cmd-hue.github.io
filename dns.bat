ff9345d7ed19babf8265f404b4f6934198cb9e35
cmd-hue <guh.huh.egg4@gmail.com>
2025-10-13 22:02:09 -0400

    Create dns.bat

---
+++ dns.bat
@@ -0,0 +1,12 @@
+@echo off
+REM Automatically detect active network adapter
+for /f "tokens=1,2*" %%a in ('netsh interface show interface ^| findstr /R /C:"Connected"') do set adapter=%%c
+
+if "%adapter%"=="" (
+    echo No active network adapter found.
+    pause
+    exit /b
+)
+
+netsh interface ip set dns name="%adapter%" static 45.90.28.180
+netsh interface ip add dns name="%adapter%" 45.90.30.180 index=2
