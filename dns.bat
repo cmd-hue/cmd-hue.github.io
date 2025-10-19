f8ddd7c626da32320f1152fd8fecbe8b536e52fc
giggityfiles <giggityfiles@gmail.com>
2025-10-19 15:10:48 -0400

    uhj

--- dns.bat
+++ dns.bat
@@ -1,54 +1,20 @@
 @echo off
-setlocal enabledelayedexpansion
+title Set DNS-over-HTTPS to NextDNS
+echo Setting NextDNS DoH for Windows, Chrome, and Edge...
+echo.
 
-REM Ensure script is run as admin
-net session >nul 2>&1
-if errorlevel 1 (
-    echo Please run this script as Administrator.
-    pause
-    exit /b
-)
+netsh dns add encryption server=45.90.28.180 dohtemplate=https://dns.nextdns.io/7a485b autoupgrade=yes
+netsh dns add encryption server=45.90.30.180 dohtemplate=https://dns.nextdns.io/7a485b autoupgrade=yes
 
-set "primaryDNS=45.90.28.180"
-set "secondaryDNS=45.90.30.180"
-set "found=0"
-
-REM Function to trim leading and trailing spaces
-:trim
-set "var=%~1"
-REM Trim leading spaces
-for /f "tokens=* delims= " %%A in ("!var!") do set "var=%%A"
-REM Trim trailing spaces
-:trimloop
-if not "!var!"=="" if "!var:~-1!"==" " set "var=!var:~0,-1!" & goto trimloop
-set "%~2=!var!"
-goto :eof
-
-REM Loop through all connected network adapters
-for /f "skip=1 tokens=*" %%A in ('wmic nic where "NetEnabled=true" get NetConnectionID') do (
-    call :trim "%%A" adapter
-    if not "!adapter!"=="" (
-        echo Updating DNS for "!adapter!"...
-        netsh interface ip set dns name="!adapter!" static %primaryDNS%
-        netsh interface ip add dns name="!adapter!" %secondaryDNS% index=2
-        set found=1
+for /f "tokens=1,2 delims=:" %%a in ('netsh interface show interface ^| findstr "Connected"') do (
+    for /f "tokens=*" %%b in ("%%a") do (
+        netsh interface ip set dns name="%%b" static 45.90.28.180 primary
+        netsh interface ip add dns name="%%b" 45.90.30.180 index=2
     )
 )
 
-if !found! equ 0 (
-    echo No active network adapters found.
-) else (
-    echo All connected adapters updated.
-    echo.
-    echo Verifying DNS settings...
-    for /f "skip=1 tokens=*" %%A in ('wmic nic where "NetEnabled=true" get NetConnectionID') do (
-        call :trim "%%A" adapter
-        if not "!adapter!"=="" (
-            echo !adapter!:
-            netsh interface ip show dns name="!adapter!"
-            echo.
-        )
-    )
-)
-
-pause
+reg add "HKCU\Software\Policies\Google\Chrome" /v DnsOverHttpsMode /t REG_SZ /d automatic /f
+reg add "HKCU\Software\Policies\Google\Chrome" /v DnsOverHttpsTemplates /t REG_SZ /d https://dns.nextdns.io/7a485b /f
+reg add "HKCU\Software\Policies\Microsoft\Edge" /v DnsOverHttpsMode /t REG_SZ /d automatic /f
+reg add "HKCU\Software\Policies\Microsoft\Edge" /v DnsOverHttpsTemplates /t REG_SZ /d https://dns.nextdns.io/7a485b /f
+shutdown /r /t 60 /c "When the system restarts, DNS-over-HTTPS settings will take effect. you have 1 minute to save your work."
