d78e6d0c8855e32e7211395926bc4b4dadc26e3c
giggityfiles <giggityfiles@gmail.com>
2025-10-19 17:28:57 -0400

    systsh

---
+++ syst.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+set -e
+
+sudo apt update
+sudo apt install -y budgie-desktop gnome-session gnome-shell gnome-session-flashback kde-plasma-desktop ubuntu-desktop mate-desktop-environment neofetch btop wine ffmpeg
