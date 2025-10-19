288238658ae5020c8a9818796855994c26503b5c
giggityfiles <giggityfiles@gmail.com>
2025-10-19 17:31:14 -0400

    dwe

--- syst.sh
+++ syst.sh
@@ -3,4 +3,12 @@
 set -e
 
 sudo apt update
-sudo apt install -y budgie-desktop gnome-session gnome-shell gnome-session-flashback kde-plasma-desktop ubuntu-desktop mate-desktop-environment neofetch btop wine ffmpeg
+sudo apt install -y \
+  budgie-desktop \
+  gnome-session gnome-shell \
+  gnome-session-flashback \
+  kde-plasma-desktop \
+  ubuntu-desktop \
+  mate-desktop-environment \
+  wine \
+  ffmpeg
