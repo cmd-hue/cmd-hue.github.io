d062dcd33ffabaa121b34805ba392fab12ac05d3
giggityfiles <giggityfiles@gmail.com>
2025-10-20 18:32:02 -0400

    Update version to 1.0.1 to use dynamic room ID

--- flood.js
+++ flood.js
@@ -1,12 +1,14 @@
-/* GGTFlood 4 BonziWorld 1.0.0 */
+/* GGTFlood 4 BonziWorld 1.0.1 */
 /* This comes with no warranty */
 setInterval(() => {
+    const roomElement = document.querySelector(".room_id");
+    const roomId = roomElement ? roomElement.textContent.trim() : "default";
     const bot = io(location.href);
     bot.emit("client", "MAIN");
     bot.emit("login", {
         passcode: "",
         name: "giggity #" + Math.floor(Math.random() * 10000),
-        room: "default",
+        room: roomId,
     });
     setTimeout(() => {
         bot.emit("talk", "tinyurl.com/ggtfiles");
