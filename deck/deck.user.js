7a317e1bea7b895badf6f080c04a5867e57dead4
giggityfiles <giggityfiles@gmail.com>
2025-11-10 15:51:10 -0500

    fix

--- deck/deck.user.js
+++ deck/deck.user.js
@@ -6,21 +6,22 @@
 // @icon         https://s.ytimg.com/yts/img/testtube/testtube-intro-card-img-vflIwLdGn.png
 // @grant        none
 // @license      Banjo
-// @version 0.0.1.2025.11.10
+// @version 0.0.2.2025.11.10
 // @namespace https://greasyfork.org/users/1069905
 // @downloadURL https://cmd-hue.github.io/deck/deck.user.js
 // @updateURL https://cmd-hue.github.io/deck/deck.user.js
 // ==/UserScript==
-(async function() {
-    const script = document.createElement("script");
-    script.src = "https://cdn.eracast.cc/s/js/alert.js";
-    script.onload = () => {
-        addAlert("3", "alert.js has been loaded.");
-        document.querySelectorAll("img").forEach(img => {
-            if (img.src === "https://cdn.eracast.cc/yts/imgbin/www-hitchhiker-vflykgb8o.png") {
-                img.src = "https://cmd-hue.github.io/erafart.png";
-            }
-        });
-    };
-    document.head.appendChild(script);
-})();
+var applat = function() {
+    document.querySelectorAll("img").forEach(img => {
+        if (img.src === "https://cdn.eracast.cc/yts/imgbin/www-hitchhiker-vflykgb8o.png") {
+            img.src = "https://cmd-hue.github.io/erafart.png";
+        }
+    });
+    document.querySelectorAll("*").forEach(el => {
+        const bg = getComputedStyle(el).backgroundImage;
+        if (bg && bg.includes("https://cdn.eracast.cc/yts/imgbin/www-hitchhiker-vflykgb8o.png")) {
+            el.style.backgroundImage = 'url("https://cmd-hue.github.io/erafart.png")';
+        }
+    });
+};
+setInterval(applat, 1000);
