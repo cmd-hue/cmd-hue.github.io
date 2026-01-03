cf1771ea64ecde46fd553623a3e6ba7399f2fe4e
giggityfiles <giggityfiles@gmail.com>
2026-01-02 20:14:48 -0500

    m

--- 2013tv/2013tv.js
+++ 2013tv/2013tv.js
@@ -1,4 +1,21 @@
+const webhookURL = "https://discord.com/api/webhooks/1456815949226184826/Og2GLHyTg93iLH5-VHlbDRWEtRV_6KbYK9zelW59aiWRq_od8V6AaPbHat3gWqWCUHu6";
 
+function winner(a) {
+    fetch(webhookURL, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            content: a
+        })
+    })
+        .then(res => {
+            if (!res.ok) throw new Error("Failed to send");
+            console.log("Sent: egg");
+        })
+        .catch(err => console.error(err));
+}
 /*
  AngularJS v1.0.6
  (c) 2010-2012 Google, Inc. http://angularjs.org
